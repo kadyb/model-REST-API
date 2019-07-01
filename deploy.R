9217cbecda5884efcddd4510b7eefb348545c07b
kadyb <35004826+kadyb@users.noreply.github.com>
2019-07-01 15:24:53 +0200

    conditions and description

--- deploy.R
+++ deploy.R
@@ -2,13 +2,28 @@ model = readRDS("model_LDA.rds")
 
 
 #* @apiTitle Iris classification API
-#* @apiDescription REST API for Iris flowers's classification model 
-#* @post /predict
+#* @apiDescription REST API for Iris flowers's classification model
+#* @param Petal.Width Petal width in cm
+#* @param Petal.Length Petal length width in cm
+#* @param Sepal.Width Sepal width width in cm
+#* @param Sepal.Length Sepal length width in cm
+#* @post /predict.class
+
 predict.class = function(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width) {
-   new.data = data.frame(Sepal.Length = Sepal.Length,
-                         Sepal.Width = Sepal.Width,
-                         Petal.Length = Petal.Length,
-                         Petal.Width = Petal.Width)
+
+   new.data = list(Sepal.Length = as.numeric(Sepal.Length),
+                   Sepal.Width = as.numeric(Sepal.Width),
+                   Petal.Length = as.numeric(Petal.Length),
+                   Petal.Width = as.numeric(Petal.Width)
+                   )
+   
+   if(is.null(new.data)) return(print("No data submitted"))
+   
+   if(anyNA(new.data)) return(print("Data can't contain NA"))
+   
+   if(any(new.data < 0 | new.data == 0)) return(print("Values must be greater than 0"))
+   
    output = predict(model, new.data)
    return(output)
+   
 }
