a475986e21d618c181a9eea5a93368a322ae20f8
kadyb <35004826+kadyb@users.noreply.github.com>
2019-07-03 10:32:37 +0200

    move description below

--- request.py
+++ request.py
@@ -2,12 +2,12 @@ import time
 import json
 import requests
 
+def request(path, service):
 """
 The function sends a request to the REST API to classify irises species.
 It is required to provide a csv file with data service address.
 The object classes are returned as a result.
 """
-def request(path, service):
     with open(path) as csvFile:
         lines = csvFile.readlines()[1:]
     csvFile.close()
