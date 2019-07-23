c3d9c3ec956a5aeac655dcfb5e8a0cfac98c40d4
kadyb <35004826+kadyb@users.noreply.github.com>
2019-07-23 09:52:42 +0200

    add logging requests

--- deploy.R
+++ deploy.R
@@ -3,12 +3,22 @@ model = readRDS("model_LDA.rds")
 
 #* @apiTitle Iris classification API
 #* @apiDescription REST API for Iris flowers's classification model
+
+#* Log information about requests
+#* @filter logger
+function(req) {
+   cat("System time:", as.character(Sys.time()), "\n",
+       "Request method:", req$REQUEST_METHOD, req$PATH_INFO, "\n",
+       "HTTP user agent:", req$HTTP_USER_AGENT, "@", req$REMOTE_ADDR, "\n")
+   
+   forward()
+}
+
 #* @param Petal.Width Petal width in cm
 #* @param Petal.Length Petal length width in cm
 #* @param Sepal.Width Sepal width width in cm
 #* @param Sepal.Length Sepal length width in cm
 #* @post /predict.class
-
 predict.class = function(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width) {
    
    new.data = list(Sepal.Length = as.numeric(Sepal.Length),
